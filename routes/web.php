dadafbd9eb5180eb28d3bfcf0824fe1375e469db
jobysooraj <jobychacko14@gmail.com>
2025-02-12 10:26:07 +0530

    route changes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ use App\Http\Controllers\Website\HomeController;
 use App\Http\Controllers\Admin\AdminLoginController;
 use App\Http\Controllers\Admin\CategoryController;
 
+use App\Http\Controllers\Website\ShopByController;
+use App\Http\Controllers\Website\BulkOrderController;
+use App\Http\Controllers\Website\AboutUsController;
+use App\Http\Controllers\Website\ContactUsController;
+
 Route::get('/welcome', function () {
     return view('welcome');
 });
@@ -24,7 +29,19 @@ Route::middleware(['auth', 'user-access:Admin','NoBackAfterLogout'])->group(func
     Route::resource('orders', OrderController::class);
 
 });
-
+Route::name('website')->group(function(){
+    // Fix the home route
+ Route::get('/', [HomeController::class, 'index'])->name('login');
+ Route::get('/shop-by', [ShopByController::class, 'index'])->name('shopBy');
+ Route::get('/bulk-order', [BulkOrderController::class, 'index'])->name('bulkOrder');
+ Route::get('/about-us', [AboutUsController::class, 'index'])->name('aboutUs');
+ Route::get('/shop-by-spices', [ShopByController::class, 'shopBySpices'])->name('shopBySpices');
+ Route::get('/shop-by-tea', [ShopByController::class, 'shopByTea'])->name('shopByTea');
+ Route::get('/shop-by-coffee', [ShopByController::class, 'shopByCoffee'])->name('shopByCoffee');
+ Route::get('/product', [ShopByController::class, 'show'])->name('product');
+ Route::get('/contact-us', [ContactUsController::class, 'index'])->name('contactUs');
+ 
+ });
 
 
 Auth::routes();
